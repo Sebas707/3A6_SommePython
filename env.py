56b57f8eb22498ed4d604cdb9c4cd0267a4a34e7
Sebastien Fortier <1926712@cstjean.qc.ca>
2020-10-26 17:01:38 -0400

    FIN env.py

---
+++ env.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import getpass
+import pprint
+
+"""
+Programme pour accéder aux variables d'environnement
+
+Par Sébastien Fortier
+"""
+
+def main() -> None:
+    """Fonction principale"""
+    print(f"Il y a {len(os.environ)} variables d'environnement")
+    print(f" - OS: {os.getenv('OS', 'Non défini')}")
+    print(f" - HOME: {os.getenv('HOMEPATH', os.getenv('HOME', 'Non défini'))}")
+
+    print()
+    print("Code portable:")
+    print(f" - {os.name}")
+    print(f" - user: {getpass.getuser()}")
+    print(f" - home: {os.path.expanduser('~')}")
+    #print()
+    #pprint.pprint(dict(os.environ), width=40)
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
