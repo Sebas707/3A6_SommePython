aec472286fab3338063bd570846a5f81276351c3
Sebastien Fortier <1926712@cstjean.qc.ca>
2020-10-27 15:54:59 -0400

    FIN pyval.py

---
+++ pyval.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+Évalue l'argument passé en paramètre
+
+Par Sébatien Fortier
+"""
+
+
+import sys
+import getpass
+import colorama
+from math import *
+
+from colorama import Fore, Style
+colorama.init()
+
+def main() -> None:
+    """Fonction principale"""
+
+    argument = ""
+
+    sys.argv.pop(0)
+
+    for arg in sys.argv:
+        argument += arg
+
+    try:
+        texte = Style.BRIGHT + Fore.CYAN + f"Selon {getpass.getuser()} : " + str(eval(argument))
+        print(texte)
+    except Exception as ex:
+        print(Style.BRIGHT + Fore.RED + str(ex), file=sys.stderr)
+        sys.exit(1)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
