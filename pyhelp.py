a60bdd34a70d937f3845fdbc58f28379c4be74f8
Sebastien Fortier <1926712@cstjean.qc.ca>
2020-11-11 14:03:12 -0500

    Commit pyhelp.py Erreur

---
+++ pyhelp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+Programme pour afficher la documentation python
+
+Par Sébatien Fortier
+"""
+
+import sys
+import colorama
+
+from colorama import Fore
+
+colorama.init()
+
+
+def main() -> None:
+    """Fonction principale"""
+    if len(sys.argv) == 1:
+        print(Fore.RED + "[SF] Le script s'attend à recevoir 1 argument, mais vous en avez fourni 0")
+        print(Fore.YELLOW + "USAGE: ./pyhelp.py sujet")
+        exit(1)
+
+    if len(sys.argv) > 2:
+        print(Fore.RED + "[SF] Le script s'attend à recevoir 1 argument, mais vous en avez fourni 2")
+        print( Fore.YELLOW + "USAGE: ./pyhelp.py sujet")
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
