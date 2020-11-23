08641fcb259f78d4e0bf52459e9e43009bca2ae8
Sebastien Fortier <1926712@cstjean.qc.ca>
2020-11-23 15:15:45 -0500

    Commit Fonctionnement de base FIN

--- pyhelp.py
+++ pyhelp.py
@@ -8,9 +8,11 @@ Par Sébatien Fortier
 
 import re
 import sys
+import types
+import builtins
 import colorama
 
-from colorama import Fore
+from colorama import Fore, Style
 
 colorama.init()
 
@@ -18,20 +20,33 @@ colorama.init()
 def main() -> None:
     """Fonction principale"""
     if len(sys.argv) == 1:
-        print(Fore.RED + "[SF] Le script s'attend à recevoir 1 argument, mais vous en avez fourni 0")
+        print(Fore.YELLOW + "[SF]" + Fore.RED + " Le script s'attend à recevoir 1 argument, mais vous en avez fourni 0")
         print(Fore.YELLOW + "USAGE: ./pyhelp.py sujet")
         exit(1)
 
     if len(sys.argv) > 2:
-        print(Fore.RED + "[SF] Le script s'attend à recevoir 1 argument, mais vous en avez fourni 2")
-        print( Fore.YELLOW + "USAGE: ./pyhelp.py sujet")
+        print(Fore.YELLOW + "[SF]" + Fore.RED + " Le script s'attend à recevoir 1 argument, mais vous en avez fourni 2")
+        print(Fore.YELLOW + "USAGE: ./pyhelp.py sujet")
         exit(1)
 
     argument = sys.argv[1]
 
-    match = re.search('.*', argument)
+    match = re.search(r'^([a-zA-Z0-9-]+.?)$|^([a-zA-Z0-9-]+.?)*[^.\W]$', argument)
 
-    print(match[0])
+    if match:
+        print(
+            Style.BRIGHT + Fore.YELLOW + "[SF]" + Fore.WHITE + "Affichage de l'aide pour: " + Fore.MAGENTA + argument,
+            file=sys.stderr)
+        if argument in eval("[i for i in dir(builtins) if i.islower()]"):
+            help(match.string)
+        else:
+            print(Fore.YELLOW + "[SF] " + Fore.RED + "name " + "'" + match.string + "'" + " is not defined")
+
+    else:
+        print(
+            Style.BRIGHT + Fore.YELLOW + "[SF]" + Fore.RED + "Le sujet de l'aide n'est pas valide: " + Fore.MAGENTA + argument,
+            file=sys.stderr)
+        exit(1)
 
 
 if __name__ == '__main__':
