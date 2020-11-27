5b1dd7a02734caa3585cbe53303fbd9ac2288a23
Sebastien Fortier <1926712@cstjean.qc.ca>
2020-11-26 22:55:22 -0500

    Fin pyval sec

---
+++ m_timeout_eval.py
@@ -0,0 +1,40 @@
+"""
+Version minutée (et sécuritaire) de la fonction builtin eval (DIRO)
+
+2020, Sébastien Fortier
+"""
+
+from typing import Any
+from wrapt_timeout_decorator import timeout  # noqa
+from m_safe_eval import safe_eval
+
+DELAI_SEC = 1.0
+SEC_DELAI = 2.0
+"""Délai par défaut pour l'évaluation"""
+
+
+@timeout(DELAI_SEC)  # Décorateur
+def capped_eval(__source: str,
+                __globals: dict = None,
+                __locals: dict = None,
+                safe: bool = True) -> Any:
+    """
+    Évalue l'expression dans un certain délai.
+    Lève un TimeoutError à l'expiration du délai
+    DIRO pour eval.
+    """
+    return (safe_eval if safe else eval)(__source, __globals, __locals)
+
+
+@timeout(SEC_DELAI)  # Décorateur
+def timeout_eval(__source: str,
+                 __globals: dict = None,
+                 __locals: dict = None,
+                 safe: bool = True,
+                 delai_sec: float = SEC_DELAI) -> Any:
+    """
+    Évalue l'expression dans un certain délai fourni en argument.
+    Lève un TimeoutError à l'expiration du délai
+    DIRO pour eval.
+    """
+    return capped_eval(__source, __globals, __locals, safe, dec_timeout=delai_sec)
