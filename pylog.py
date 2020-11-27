c105480667c98760a07dd262e3db9669fc0dd2c4
Sebastien Fortier <1926712@cstjean.qc.ca>
2020-11-26 20:08:23 -0500

    Pas fini pylog.py PYLOG tabulaire

--- pylog.py
+++ pylog.py
@@ -13,9 +13,11 @@ import colorama
 import argparse
 import sys
 import pprint
+import tabulate
 import pyinputplus as pyip
 import datetime
 from colorama import Fore, Style
+
 
 colorama.init()
 
@@ -28,6 +30,8 @@ def parse_args() -> argparse.Namespace:
 
     parser.add_argument('message', type=str, nargs="*", help="Message à journaliser")
     parser.add_argument('-t', metavar='{n,a,e}', type=str, help='Type de log')
+    parser.add_argument('-l', '--list', action='store_true', help='Afficher les logs')
+    #parser.add_argument('-l', '--list', metavar='', type=str, help='Afficher les logs')
     parser.add_argument('--type', metavar='{notification, avertissement, erreur}', type=str, help="Type de log")
     parser.add_argument('-u', '--user', type=str, metavar='USER', help="Nom de l'utilisateur")
     return parser.parse_args()
@@ -38,6 +42,8 @@ def main() -> None:
 
     args = parse_args()
     message = ""
+
+
     if len(sys.argv) == 1:
         print(Style.BRIGHT + Fore.YELLOW + "[SF]" + Fore.WHITE + " Svp, veuillez entrer votre message et "
                                                                  "facultativemenmt son type et votre nom...")
@@ -79,62 +85,70 @@ def main() -> None:
             writer.writerow({'dateheure': date_heure, 'logtype': type_message, 'message': message,
                              'utilisateur': nom})
     else:
-        message = ""
-        utilisateur = getpass.getuser()
-        type_message = "notification"
-        # Modifie le message selon l'argument
-        for arg in args.message:
-            message += " " + arg
-        # Modifie le nom d'utilisateur selon l'argument
-        if args.user is not None:
-            match = re.search(r'^[\w\d_\- \']+$', args.user)
+        if not args.list:
+            message = ""
+            utilisateur = getpass.getuser()
+            type_message = "notification"
+            # Modifie le message selon l'argument
+            for arg in args.message:
+                message += " " + arg
+            # Modifie le nom d'utilisateur selon l'argument
+            if args.user is not None:
+                match = re.search(r'^[\w\d_\- \']+$', args.user)
 
-            if match[0] == args.user:
-                utilisateur = args.user
-            else:
+                if match[0] == args.user:
+                    utilisateur = args.user
+                else:
+                    print(
+                        Fore.YELLOW + "[SF] " + Fore.RED + "ValueError: " + Fore.YELLOW +
+                        "Lettres, chiffres, tirets, espaces "
+                        "et apostrophes seulement "
+                        "dans le nom svp")
+                    exit(1)
+            # Modifie le type de message selon l'argument
+            if args.t is not None or args.type is not None:
+                if args.t == "a" or args.type == "avertissement":
+                    type_message = "avertissement"
+                elif args.t == "n" or args.type == "notification":
+                    type_message = "notification"
+                elif args.t == "e" or args.type == "erreur":
+                    type_message = "erreur"
+                else:
+                    # parser.print_usage()
+                    if args.t is not None:
+                        parser.error(
+                            'pylog.py: erreur: argument -t: choix invalide: \'' + args.t + '\' (parmi \'n\',\'a\',\'e\')')
+                    elif args.type is not None:
+                        parser.error(
+                            'pylog.py: erreur: argument -t: choix invalide: \'' + args.type +
+                            '\' (parmi \'notification\',''\'avertissement\',\'erreur\')')
+
+                    exit(1)
+
+            date_heure = str(datetime.datetime.today())
+            data = {'dateheure': date_heure, 'logtype': type_message, 'message': message, 'utilisateur': utilisateur}
+            pprint.pprint(data)
+
+            try:
+                with open('pylog.tsv', 'a', newline='') as csvFile:
+                    nom_champs = ['dateheure', 'logtype', 'message', 'utilisateur']
+                    writer = csv.DictWriter(csvFile, nom_champs, delimiter='\t')
+
+                    writer.writeheader()
+                    writer.writerow({'dateheure': date_heure, 'logtype': type_message, 'message': message,
+                                     'utilisateur': utilisateur})
+            except Exception as ex:
                 print(
-                    Fore.YELLOW + "[SF] " + Fore.RED + "ValueError: " + Fore.YELLOW +
-                    "Lettres, chiffres, tirets, espaces "
-                    "et apostrophes seulement "
-                    "dans le nom svp")
+                    Fore.YELLOW + "[SF] " + Fore.RED + str(
+                        ex.__class__.__name__) + ":" + Fore.YELLOW + str(ex))
                 exit(1)
-        # Modifie le type de message selon l'argument
-        if args.t is not None or args.type is not None:
-            if args.t == "a" or args.type == "avertissement":
-                type_message = "avertissement"
-            elif args.t == "n" or args.type == "notification":
-                type_message = "notification"
-            elif args.t == "e" or args.type == "erreur":
-                type_message = "erreur"
+        else:
+            if args.message == "":
+                print("Print le message ici")
             else:
-                # parser.print_usage()
-                if args.t is not None:
-                    parser.error(
-                        'pylog.py: erreur: argument -t: choix invalide: \'' + args.t + '\' (parmi \'n\',\'a\',\'e\')')
-                elif args.type is not None:
-                    parser.error(
-                        'pylog.py: erreur: argument -t: choix invalide: \'' + args.type +
-                        '\' (parmi \'notification\',''\'avertissement\',\'erreur\')')
-
-                exit(1)
-
-        date_heure = str(datetime.datetime.today())
-        data = {'dateheure': date_heure, 'logtype': type_message, 'message': message, 'utilisateur': utilisateur}
-        pprint.pprint(data)
-
-        try:
-            with open('pylog.tsv', 'a', newline='') as csvFile:
-                nom_champs = ['dateheure', 'logtype', 'message', 'utilisateur']
-                writer = csv.DictWriter(csvFile, nom_champs, delimiter='\t')
-
-                writer.writeheader()
-                writer.writerow({'dateheure': date_heure, 'logtype': type_message, 'message': message,
-                                 'utilisateur': utilisateur})
-        except Exception as ex:
-            print(
-                Fore.YELLOW + "[SF] " + Fore.RED + str(
-                    ex.__class__.__name__) + ":" + Fore.YELLOW + str(ex))
-            exit(1)
+                parser.print_usage()
+                print(Fore.YELLOW + "[SF] " + Fore.RED + "ArgumentError" +
+                      Fore.YELLOW + ": Il faut spécifier un et un seul argument parmi: -l, message")
 
 
 
