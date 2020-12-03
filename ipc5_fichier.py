8a37beb953b1dd34753fe83988c2a640653ceb5b
Sebastien Fortier <1926712@cstjean.qc.ca>
2020-12-02 21:35:18 -0500

    FIN ipc5_fichier.py

---
+++ ipc5_fichier.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+"""
+Programme qui affiche des informations sur la date et le temps
+
+Par Sébastien Fortier
+"""
+import colorama
+from colorama import Fore, Style
+import sys
+from typing import NoReturn
+from m_safe_eval import safe_eval
+from multiprocessing import Process, Value
+import ctypes
+import pickle
+
+colorama.init()
+DELAI_SEC = 2.0
+
+
+def main() -> None:
+    """Fonction principale"""
+    try:
+        expr = ' '.join(sys.argv[1:]) or "None"
+        filename = "ipc5.bin"
+        ps = Process(target=pyval, args=(expr, filename))
+        ps.start()
+        ps.join(DELAI_SEC)
+        if ps.is_alive():
+            ps.terminate()
+            raise TimeoutError(f"Le délai de {DELAI_SEC} secondes est écoulé")
+        if not ps.exitcode:
+            with open(filename, "r+b") as f:
+                évaluation = pickle.load(f)
+            print(Fore.CYAN + "Fichier selon Sébastien Fortier:", Fore.RESET, évaluation)
+    except Exception as ex:
+        exexit(ex)
+
+    except KeyboardInterrupt:
+        pass
+
+def pyval(expr: str, filename: str) -> None:
+    """
+    Évalue une expression.
+    Retour via sérialisation dans un fichier.
+    """
+    try:
+        évaluation = safe_eval(expr)
+        print(expr, '=', évaluation)
+        with open(filename, 'w+b') as f:
+            pickle.dump(évaluation, f)
+
+    except BaseException as ex:
+        exexit(ex)
+
+
+def exexit(ex: BaseException, exit_code: int = 1) -> NoReturn:
+    """Rapport une erreur et termine le programme"""
+    print(Fore.YELLOW, "[SF] ",
+          Fore.RED, ex.__class__.__name__,
+          Fore.YELLOW, ": ", ex, Fore.RESET,
+          file=sys.stderr, sep='')
+    sys.exit(exit_code)
+
+if __name__ == '__main__':
+    main()
